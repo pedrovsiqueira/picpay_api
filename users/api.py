ca1fa3766a4db491a19e013df7609581cacba21e
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-07-10 16:59:28 -0300

    feat: create initial users endpoint

--- users/api.py
+++ users/api.py
@@ -1,8 +1,15 @@
 from ninja import Router
+from .schemas import UserSchema
+from .models import User
+from django.contrib.auth.hashers import make_password
 
 users_router = Router()
 
 
 @users_router.post('/', response={200: dict})
-def create_user(request):
+def create_user(request, user: UserSchema):
+    user = User(**user.dict())
+    user.password = make_password(user.password)
+    user.save()
+
     return {'ok': 'ok'}
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 class User(AbstractUser):
     cpf = models.CharField(max_length=14, unique=True)
     email = models.EmailField(unique=True)
-    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    amount = models.DecimalField(max_digits=15, decimal_places=2, default=Decimal('0.00'))
 
     def save(self, *args, **kwargs):
         self.cpf = self.cpf.replace('.', '').replace('-', '')
