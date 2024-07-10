910599f68b4853c9e8c85bd7aed661a8bacaae77
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-07-10 17:52:05 -0300

    feat: create people and company roles

--- users/api.py
+++ users/api.py
@@ -1,22 +1,24 @@
 from ninja import Router
-from .schemas import UserSchema
+from .schemas import TypeUserSchema
 from .models import User
 from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
+from rolepermissions.roles import assign_role
 
 users_router = Router()
 
 
 @users_router.post('/', response={200: dict, 400: dict, 500: dict})
-def create_user(request, user: UserSchema):
-    user = User(**user.dict())
-    user.password = make_password(user.password)
+def create_user(request, type_user_schema: TypeUserSchema):
+    user = User(**type_user_schema.user.dict())
+    user.password = make_password(type_user_schema.user.password)
     try:
         user.full_clean()
         user.save()
+        assign_role(user, type_user_schema.type_user.type)
     except ValidationError as e:
         return 400, {'errors': e.message_dict}
     except Exception as e:
         return 500, {'errors': 'Internal server error, contact an admin'}
 
-    return {'ok': 'ok'}
+    return {'user_id': user.id}
